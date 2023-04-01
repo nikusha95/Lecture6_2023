b0ce2ec421f727ddafb1d97d381abe4f3b3f0083
nikusha95 <nikasanaia11@yahoo.com>
2023-04-01 11:19:19 +0400

    init

---
+++ Enums/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Enums;
+
+int x;
+
+x = 10;
+
+PersonType type1;
+
+type1 = (PersonType)100; //Teacher
+
+Console.WriteLine(type1);
+
+
+string type = Console.ReadLine();
+PersonType typeEnum = Enum.Parse<PersonType>(type);
+
+Console.WriteLine(typeEnum);
+
+Console.WriteLine(PersonType.Teacher);
+Console.WriteLine((int)PersonType.Teacher);
